6a543ef8dfbab328a87010b8bb1f2d954e59d878
Halid Lihovac <87331502+Halcooo@users.noreply.github.com>
2021-12-24 20:15:47 +0100

    Add files via upload

---
+++ matrica unazad/script.js
@@ -0,0 +1,17 @@
+/*
+******
+*****
+****
+***
+**
+*
+
+
+*/
+var visina = 16;
+for (let i = 0; i < visina; i++) {
+  for (let j = visina; j > i; j--) {
+    document.write("*");
+  }
+  document.write("<br>");
+}
