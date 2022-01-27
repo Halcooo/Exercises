9660f5416eeb1c521b25fe899e7a545494bcd32c
Halid Lihovac <87331502+Halcooo@users.noreply.github.com>
2022-01-27 20:36:26 +0100

    Add files via upload

--- Vjezba 3/script.js
+++ Vjezba 3/script.js
@@ -1,196 +1,184 @@
-var users = [
-  {
-    name: "John Doe",
-    email: "john.doe@gmail.com",
-    address: "Grbavicka br12",
-    username: "johny",
-    password: "123456",
-  },
-];
+var users = [{
+    name: 'John Doe',
+    email: 'john.doe@gmail.com',
+    address: 'Grbavicka br12',
+    username: 'johny',
+    password: '123456',
+}];
 
 var loggedUser = {};
 
+
 var allBlogs = [];
 
-var signupForm = document.getElementById("signup-form");
-signupForm.style.display = "none";
+var signupForm = document.getElementById('signup-form');
+signupForm.style.display = 'none';
 
 function isUserLogged() {
-  var userData = localStorage.getItem("loggedUser");
-  if (userData) {
-    var user = JSON.parse(userData);
-    login(user.email, user.password);
-  }
+    var userData = localStorage.getItem('loggedUser');
+    if (userData) {
+        var user = JSON.parse(userData);
+        login(user.email, user.password);
+    }
 }
 
 isUserLogged();
 
-function login(p_email, p_password) {
-  var email = p_email || document.getElementById("email").value;
-  var password = p_password || document.getElementById("password").value;
-  var usersData = localStorage.getItem("users");
-  if (usersData) {
-    users = JSON.parse(usersData);
-  }
-  for (var user of users) {
-    if (
-      (email === user.email || email === user.username) &&
-      password === user.password
-    ) {
-      var loginForm = document.getElementById("login-form");
-      loginForm.style.display = "none";
-      var nav = document.querySelector(".nav");
-      nav.style.display = "block";
-      var name = document.getElementById("user-name");
-      name.innerHTML = user.name;
-      loggedUser = user;
-      localStorage.setItem("loggedUser", JSON.stringify(loggedUser));
-      clearValue("email");
-      clearValue("password");
-    } else {
-      var errorMsg = document.querySelector("#login-form .error-msg");
-      errorMsg.style.display = "block";
-    }
-  }
 
-  displayBlog();
+function login(p_email, p_password) {
+    var email = p_email || document.getElementById('email').value;
+    var password = p_password || document.getElementById('password').value;
+    var usersData = localStorage.getItem('users');
+    if (usersData) {
+        users = JSON.parse(usersData);
+    }
+    for (var user of users) {
+        if ((email === user.email || email === user.username) && password === user.password) {
+            var loginForm = document.getElementById('login-form');
+            loginForm.style.display = 'none';
+            var nav = document.querySelector('.nav');
+            nav.style.display = 'block';
+            var name = document.getElementById('user-name');
+            name.innerHTML = user.name;
+            loggedUser = user;
+            localStorage.setItem('loggedUser', JSON.stringify(loggedUser));
+            clearValue('email');
+            clearValue('password');
+        } else {
+            var errorMsg = document.querySelector('#login-form .error-msg');
+            errorMsg.style.display = 'block';
+        }
+    }
+
+    displayBlog();
 }
 
 function loginOnEnter(e) {
-  if (e.keyCode === 13) {
-    console.log("User je pritisnuo enter");
-    login();
-  }
+    if (e.keyCode === 13) {
+        console.log('User je pritisnuo enter');
+        login();
+    }
 }
 
 function logout() {
-  var loginForm = document.getElementById("login-form");
-  loginForm.style.display = "block";
-  var nav = document.querySelector(".nav");
-  nav.style.display = "none";
-  document.querySelector("#login-form .error-msg").style.display = "none";
-  localStorage.removeItem("loggedUser");
+    var loginForm = document.getElementById('login-form');
+    loginForm.style.display = 'block';
+    var nav = document.querySelector('.nav');
+    nav.style.display = 'none';
+    document.querySelector('#login-form .error-msg').style.display = 'none';
+    localStorage.removeItem('loggedUser');
 }
 
 function goToSignupForm() {
-  var loginForm = document.getElementById("login-form");
-  loginForm.style.display = "none";
-  signupForm.style.display = "block";
+    var loginForm = document.getElementById('login-form');
+    loginForm.style.display = 'none';
+    signupForm.style.display = 'block';
 }
 
 function goToLoginForm() {
-  var loginForm = document.getElementById("login-form");
-  loginForm.style.display = "block";
-  signupForm.style.display = "none";
+    var loginForm = document.getElementById('login-form');
+    loginForm.style.display = 'block';
+    signupForm.style.display = 'none';
 }
 
 function registerNow() {
-  console.log("logika za dodavanje korisnika");
-  var name = getValue("name");
-  var email = getValue("su-email");
-  var address = getValue("address");
-  var username = getValue("username");
-  var password = getValue("su-password");
-  if (
-    name === "" ||
-    email === "" ||
-    address === "" ||
-    username === "" ||
-    password === ""
-  ) {
-    return alert("Unesite sve podatke");
-  }
-  var user = {
-    name,
-    email,
-    address,
-    username,
-    password,
-  };
-  users.push(user);
-  localStorage.setItem("users", JSON.stringify(users));
-  clearValue("name");
-  clearValue("su-email");
-  clearValue("address");
-  clearValue("username");
-  clearValue("su-password");
+    console.log('logika za dodavanje korisnika');
+    var name = getValue('name');
+    var email = getValue('su-email');
+    var address = getValue('address');
+    var username = getValue('username');
+    var password = getValue('su-password');
+    if (name === '' || email === '' || address === '' || username === '' || password === '') {
+        return alert('Unesite sve podatke')
+    }
+    var user = {
+        name,
+        email,
+        address,
+        username,
+        password
+    };
+    users.push(user);
+    localStorage.setItem('users', JSON.stringify(users))
+    clearValue('name');
+    clearValue('su-email');
+    clearValue('address');
+    clearValue('username');
+    clearValue('su-password');
 
-  goToLoginForm();
+    goToLoginForm();
 }
 
 function getValue(id) {
-  return document.getElementById(id).value;
+    return document.getElementById(id).value;
 }
 
 function clearValue(id) {
-  document.getElementById(id).value = "";
+    document.getElementById(id).value = '';
 }
 
 function postBLog() {
-  var blogTitle = getValue("blog-title");
-  var blogDesc = getValue("blog-desc");
+    var blogTitle = getValue('blog-title');
+    var blogDesc = getValue('blog-desc');
 
-  if (blogTitle === "" || blogDesc === "") {
-    return alert("Popunite sve podatke");
-  }
+    if (blogTitle === '' || blogDesc === '') {
+        return alert('Popunite sve podatke')
+    }
 
-  var blog = {
-    blogTitle,
-    blogDesc,
-    postDate: new Date(),
-    author: loggedUser.name,
-  };
+    var blog = {
+        blogTitle,
+        blogDesc,
+        postDate: new Date(),
+        author: loggedUser.name
+    };
 
-  allBlogs.push(blog);
-  localStorage.setItem("blogs", JSON.stringify(allBlogs));
-  displayBlog();
+    allBlogs.push(blog);
+    localStorage.setItem('blogs', JSON.stringify(allBlogs));
+    displayBlog();
 
-  clearValue("blog-title");
-  clearValue("blog-desc");
+    clearValue('blog-title');
+    clearValue('blog-desc');
 }
 
 function displayBlog() {
-  var blogsData = localStorage.getItem("blogs");
-  if (blogsData) {
-    allBlogs = JSON.parse(blogsData);
-  }
-  var publishedBlogs = document.getElementById("published-blogs");
-  publishedBlogs.innerHTML = "";
-  for (var blog of allBlogs) {
-    
-      var trazi = document.getElementById("blogSearch").value;
-      
-      if (blog.blogTitle.toUpperCase().includes(trazi.toUpperCase()) || blog.blogDesc.toUpperCase().includes(trazi.toUpperCase())) {
-    var h3 = document.createElement("h3");
-    h3.innerHTML = blog.blogTitle;
-    h3.classList.add("blog-title");
-    var div = document.createElement("div");
-    div.classList.add("posted-blog");
-    var p = document.createElement("p");
-    p.innerHTML = blog.blogDesc;
-    var span = document.createElement("span");
-    span.innerHTML = `Author: ${blog.author}`;
-    var datum = document.createElement("i");
-    datum.style.paddingLeft = "30px";
-    datum.innerHTML = new Date(blog.postDate).toLocaleString();
-    div.appendChild(p);
-    div.appendChild(span);
-    div.appendChild(datum);
-    publishedBlogs.appendChild(h3);
-    publishedBlogs.appendChild(div);
-
-      
+    var blogsData = localStorage.getItem('blogs');
+    if (blogsData) {
+        allBlogs = JSON.parse(blogsData);
     }
-    
-
-  }
-
-
+    renderBlogs(allBlogs);
 }
 
+function searchBlogs(e) {
+    var searchBy = e.target.value;
+    var filteredBlogs = [];
+    for (var blog of allBlogs) {
+        if (blog.blogTitle.toLowerCase().indexOf(searchBy.toLowerCase()) > -1) {
+            filteredBlogs.push(blog);
+        }
+    }
+    renderBlogs(filteredBlogs);
+}
 
-
-
-function search(){
-    displayBlog();
+function renderBlogs(blogs) {
+    var publishedBlogs = document.getElementById('published-blogs')
+    publishedBlogs.innerHTML = '';
+    for (var blog of blogs) {
+        var h3 = document.createElement('h3');
+        h3.innerHTML = blog.blogTitle;
+        h3.classList.add('blog-title');
+        var div = document.createElement('div');
+        div.classList.add('posted-blog');
+        var p = document.createElement('p');
+        p.innerHTML = blog.blogDesc;
+        var span = document.createElement('span');
+        span.innerHTML = `Author: ${blog.author}`;
+        var datum = document.createElement('i');
+        datum.style.paddingLeft = '30px';
+        datum.innerHTML = new Date(blog.postDate).toLocaleString();
+        div.appendChild(p);
+        div.appendChild(span);
+        div.appendChild(datum);
+        publishedBlogs.appendChild(h3);
+        publishedBlogs.appendChild(div)
+    }
 }
